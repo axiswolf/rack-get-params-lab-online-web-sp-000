714761748fe713e81da8d6b0b93355b1100002af
Karen V <karenvo@usc.edu>
2019-09-21 04:22:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,19 +7,20 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.math(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |cart_item|
-        resp.write "#{cart_item}\n"
-      end
+        @@items.each do |item|
+          resp.write "#{item}\n"
+        end
+      elsif req.path.match(/search/)
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+      elsif req.path.match(/cart/)
+        if @@cart.empty?
+          resp.write "Your cart is empty"
+        else
+          @@cart.each do |cart_item|
+            resp.write "#{cart_item}\n"
+          end
+        end
     # elsif req.path.match(/add/)
     #   item = req.params["item"]
     #   if @@items.include?(item)
