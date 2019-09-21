f8f592bcc28e8538d38c6cc2c27e0d2935dfa96e
Karen V <karenvo@usc.edu>
2019-09-21 04:16:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,25 +6,32 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
-    #if req.path.match(/items/)
-    # Seeing all items
-      # @@items.each do |item|
-      #   resp.write "#{item}\n"
-      # end
-    if req.path.match(/cart/)
+    if req.path.math(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    # elsif req.path.match(/search/)
-    #   search_term = req.params["q"]
-    #   resp.write handle_search(search_term)
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart_item|
+          resp.write "#{cart_item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
     else
-      resp.write "Your cart is empty"
-    end
-    resp.finish
+      resp.write "Path Not Found"
+      resp.finish
   end
-
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
