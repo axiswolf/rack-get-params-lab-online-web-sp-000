07a2a63f441ce00e828927c9a89dd2755defdac2
Karen V <karenvo@usc.edu>
2019-09-21 04:18:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -28,6 +28,7 @@ class Application
         resp.write "added #{item}"
       else
         resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
       resp.finish
