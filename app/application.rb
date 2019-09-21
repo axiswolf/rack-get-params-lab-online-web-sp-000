0c1066220243e83846c27628318e73a61fbfa64e
Karen V <karenvo@usc.edu>
2019-09-21 04:03:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,8 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    #if req.path.match(/items/)
+    # Seeing all items
       @@items.each do |item|
         resp.write "#{item}\n"
       end
