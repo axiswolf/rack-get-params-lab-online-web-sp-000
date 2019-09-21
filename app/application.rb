01eaf24e79caed0d5f9bbb0ce5fe29cc7c37de2f
Karen V <karenvo@usc.edu>
2019-09-21 04:27:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,10 +31,11 @@ class Application
         end
       else
         resp.write "Path Not Found"
-        resp.finish
       end
+      resp.finish
     end
 
+    
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
